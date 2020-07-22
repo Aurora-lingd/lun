6542679318aa059baa8b96b760dee258084db364
tracy <843358687@qq.com>
2020-07-22 23:16:34 +0800

    默认为 phone样式

--- src/col.vue
+++ src/col.vue
@@ -26,7 +26,6 @@
       offset:{
         type:[Number,String]
       },
-      phone:{type:Object, validator},
       ipad:{type:Object, validator},
       narrowPc:{type:Object, validator},
       pc:{type:Object, validator},
@@ -39,11 +38,10 @@
     },
     computed:{
       colClass(){
-        let {span,offset,phone,ipad,narrowPc,pc,widePc} = this
+        let {span,offset,ipad,narrowPc,pc,widePc} = this
         return [
           span &&`col-${span}`,
           offset &&`offset-${offset}`,
-          ...(phone && [`col-phone-${phone.span}`]),
           ...(ipad && [`col-ipad-${ipad.span}`]),
           ...(narrowPc && [`col-narrowPc-${narrowPc.span}`]),
           ...(pc && [`col-pc-${pc.span}`]),
@@ -68,20 +66,6 @@
     @for $n from 1 through 24{
       &.#{$class-prefix}#{$n} {
         margin-left: ($n/24) * 100%;
-      }
-    }
-    @media (max-width: 576px) {
-      $class-prefix: col-phone-;
-      @for $n from 1 through 24{
-        &.#{$class-prefix}#{$n} {
-          width: ($n/24) * 100%;
-        }
-      }
-      $class-prefix: offset-phone-;
-      @for $n from 1 through 24{
-        &.#{$class-prefix}#{$n} {
-          margin-left: ($n/24) * 100%;
-        }
       }
     }
     @media (min-width:577px )and (max-width: 768px) {
