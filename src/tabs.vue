b16066dc87e99272cb8b721f7e55151e258b9ab7
tracy <843358687@qq.com>
2020-07-28 10:37:33 +0800

    添加部分测试用例

--- src/tabs.vue
+++ src/tabs.vue
@@ -31,11 +31,14 @@
       }
     },
     mounted(){
+      if (this.$children.length === 0){
+        console && console.warn && console.warn('tabs的子组件应该是tabs-head和tabs-nav，但你没有写子组件')
+      }
       this.$children.forEach((vm)=>{
-       if (vm.$options.name === 'WeiTabsHead'){
-         vm.$children.forEach((childVm)=>{
-           if (childVm.$options.name === 'WeiTabsItem' && childVm.name === this.selected){
-             this.eventBus.$emit('update:selected',this.selected,childVm)
+        if (vm.$options.name === 'WeiTabsHead'){
+          vm.$children.forEach((childVm)=>{
+            if (childVm.$options.name === 'WeiTabsItem' && childVm.name === this.selected){
+              this.eventBus.$emit('update:selected',this.selected,childVm)
            }
          })
        }
