4942748f70f7575ca4e59526b2fc0960b18b6f95
tracy <843358687@qq.com>
2020-07-31 21:55:32 +0800

    修改 官网首页

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,12 +3,18 @@ module.exports = {
   title: '微光UI',
   description: '一个好用的UI框架',
   themeConfig: {
+    nav:[
+      {text:'主页',link:'/'},
+      {text:'文档',link:'/install/'},
+    ],
     sidebar: [
       {
+        collapsable: false,
         title: '入门',
         children: ['/install/', '/get-started/',]
       },
       {
+        collapsable: false,
         title: '组件',
         children: [
           '/components/button',
--- docs/README.md
+++ docs/README.md
@@ -1,5 +1,17 @@
-# 微光 UI 官网
-
-微光 UI 是一个简约的 UI 框架，提供了 ...... 等常用组件，适合移动端和 pc 端使用。
+---
+home: true
+heroImage: https://i.loli.net/2020/07/31/f3XmP9Q68YRpjJr.png
+heroText: 微光
+tagline: 一个基于 Vue 的简单 UI
+actionText: 快速上手 →
+actionLink: /install/
+features:
+- title: 简约
+  details: 以极简风格的设计，满足使用者普通需求。
+- title: Vue驱动
+  details: 基于 Vue 的 UI 组件。
+- title: 持续集成
+  details: 进行了自动化测试，保证每个组件质量。
+---
 
 
