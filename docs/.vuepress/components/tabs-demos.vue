d21303783d5ba078d684ee2162eba5d14fa50ae3
tracy <843358687@qq.com>
2020-08-01 20:43:11 +0800

    创建 tabs.md文档

---
+++ docs/.vuepress/components/tabs-demos.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+   <w-tabs :selected="selected">
+    <w-tabs-head>
+      <w-tabs-item name="1" disabled>标题1</w-tabs-item>
+      <w-tabs-item name="2">标题2</w-tabs-item>
+      <w-tabs-item name="3">标题3</w-tabs-item>
+    </w-tabs-head>
+     <w-tabs-body>
+       <w-tabs-pane name="1">标题1相关内容</w-tabs-pane>
+       <w-tabs-pane name="2">标题2相关内容</w-tabs-pane>
+       <w-tabs-pane name="3">标题3相关内容</w-tabs-pane>
+     </w-tabs-body>
+   </w-tabs>
+  </div>
+</template>
+<script>
+  import Tabs from '../../../src/tabs'
+  import TabsBody from '../../../src/tabs-body'
+  import TabsHead from '../../../src/tabs-head'
+  import TabsItem from '../../../src/tabs-item'
+  import TabsPane from '../../../src/tabs-pane'
+  export default {
+    components: {
+      'w-tabs':Tabs,
+      'w-tabs-head':TabsHead,
+      'w-tabs-body':TabsBody,
+      'w-tabs-item':TabsItem,
+      'w-tabs-pane':TabsPane,
+    },
+    data(){
+      return{
+        selected:'2',
+      }
+    }
+  }
+</script>
--- docs/components/tabs.md
+++ docs/components/tabs.md
@@ -2,7 +2,47 @@
 title:Tabs - 标签
 ---
 
-# 标签
+# Tabs 标签页
 
-使用方法
+## 功能介绍
+可根据标签选择对应的内容
+
+* **selected**: 设置默认的选项，必须填。
+
+* **name**：用来关联对应内容的，必须填。
+
+* **disabled**：设置禁用的选项
+
+
+## 常用例子
+<br>
+
+**预览样式**
+
+<ClientOnly>
+<tabs-demos></tabs-demos>
+</ClientOnly>
+
+<br>
+
+**使用方法**
+```html
+data(){
+  return{
+    selected:'2',
+  }
+}
+<w-tabs :selected="selected">
+<w-tabs-head>
+  <w-tabs-item name="1" disabled>标题1</w-tabs-item>
+  <w-tabs-item name="2">标题2</w-tabs-item>
+  <w-tabs-item name="3">标题3</w-tabs-item>
+</w-tabs-head>
+ <w-tabs-body>
+   <w-tabs-pane name="1">标题1相关内容</w-tabs-pane>
+   <w-tabs-pane name="2">标题2相关内容</w-tabs-pane>
+   <w-tabs-pane name="3">标题3相关内容</w-tabs-pane>
+ </w-tabs-body>
+</w-tabs>
+```
 
