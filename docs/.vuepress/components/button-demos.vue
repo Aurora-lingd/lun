a71b8346f1e5880f438c441f94702c8aefe976f7
tracy <843358687@qq.com>
2020-08-03 09:46:14 +0800

    修改 button 文档

--- docs/.vuepress/components/button-demos.vue
+++ docs/.vuepress/components/button-demos.vue
@@ -1,7 +1,5 @@
 <template>
   <div>
-    <div class="style">
-      <div class="style"><b>预览样式</b></div>
       <w-button>默认按钮</w-button>
       <w-button icon="settings">设置</w-button>
       <w-button :loading="true" >加载</w-button>
@@ -10,33 +8,10 @@
       <w-button icon="download" icon-position="right">下载</w-button>
       <w-button disabled>禁用</w-button>
     </div>
-    <div class="style">
-      <div><b>使用方法</b></div>
-      <pre><code>{{content}}</code></pre>
-    </div>
-  </div>
 </template>
 <script>
   import Button from '../../../src/button'
   export default {
     components:{'w-button':Button},
-    data(){
-      return{
-        content:`
-        <w-button>默认按钮</w-button>
-      <w-button icon="settings">设置</w-button>
-      <w-button :loading="true" >加载</w-button>
-      <w-button icon="thumbs-up">点赞</w-button>
-      <w-button icon="upload">上传</w-button>
-      <w-button icon="download" icon-position="right">下载</w-button>
-      <w-button disabled>禁用</w-button>
-        `.replace(/\t+| +/g,'').trim(),
-      }
-    }
   }
 </script>
-<style lang="scss" scoped>
-  .style{
-    padding: 16px 0;
-  }
-</style>
--- docs/components/button.md
+++ docs/components/button.md
@@ -1,21 +1,52 @@
 ---
-title:Button
+title:按钮 Button
 ---
 
-# 按钮
+# Button 按钮
 
 ## 功能介绍
 
 提供了单个按钮和按钮组，可添加图标、设置图标位置功能
-
 ## 单个按钮
+<br>
+
+**预览样式**
 
 <ClientOnly>
 <button-demos></button-demos>
 </ClientOnly>
 
+<br>
+
+**使用方法**
+```html
+<w-button>默认按钮</w-button>
+<w-button icon="settings">设置</w-button>
+<w-button :loading="true" >加载</w-button>
+<w-button icon="thumbs-up">点赞</w-button>
+<w-button icon="upload">上传</w-button>
+<w-button icon="download" icon-position="right">下载</w-button>
+<w-button disabled>禁用</w-button>
+```
+
 ## 按钮组
+<br>
+
+**预览样式**
+
 <ClientOnly>
 <button-group-demos></button-group-demos>
 </ClientOnly>
+
+<br>
+
+**使用方法**
+```html
+<w-button-group>
+  <w-button icon="left">上一页</w-button>
+  <w-button>更多</w-button>
+  <w-button icon="right" icon-position="right">下一页</w-button>
+</w-button-group>
+```
+
 
